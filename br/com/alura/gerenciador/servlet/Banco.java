c0518cb80e62e80e9c5c40d611b6be415474ecbf
Giuli Ilustre <ilustre.giuli@gmail.com>
2021-06-23 22:33:27 -0300

    Criando servlet para mostrar a empresa e depois metodo em Banco para buscar uma empresa pelo id

--- br/com/alura/gerenciador/servlet/Banco.java
+++ br/com/alura/gerenciador/servlet/Banco.java
@@ -44,4 +44,13 @@ public class Banco {
 		}
 		
 	}
+
+	public Empresa buscaId(Integer id) {
+		for (Empresa empresa : lista) {
+			if(empresa.getId() == id) {
+				return empresa;
+			}
+		}
+		return null;
+	}
 }
